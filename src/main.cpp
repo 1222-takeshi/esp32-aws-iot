0e5651b45c439ffe2f15474ea3b3f42b18473f62
Takeshi Miura <57553950+1222-takeshi@users.noreply.github.com>
2022-02-11 20:53:06 +0900

    create OTA library (#5)

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,18 @@
 
 #include "ultrasonic/ultrasonic.h"
 #include "Connect_AWS/Connect_AWS.h"
+#include "OTA/OTA.h"
 
 // The MQTT topics that this device should publish/subscribe
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+#define HOSTNAME "myesp32"
 #define Trigger_Pin 13
 #define Echo_Pin 12
 float distance;
 UltraSonic ultrasonic = UltraSonic(Trigger_Pin, Echo_Pin);
 ConnectAWS connect_aws = ConnectAWS(AWS_IOT_PUBLISH_TOPIC, AWS_IOT_SUBSCRIBE_TOPIC);
+OTA ota = OTA(HOSTNAME);
 
 void publishMessage()
 {
@@ -18,7 +21,7 @@ void publishMessage()
   doc["time"] = millis();
   doc["ultrasonic"] = distance;
   char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer); // print to client
+  serializeJson(doc, jsonBuffer);  // print to client
 
   connect_aws._client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
@@ -28,10 +31,9 @@ void setup()
   Serial.begin(115200);
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 
   connect_aws.connectToAWS();
+  ota.setupOTA();
 }
 
 void loop()
@@ -40,5 +42,6 @@ void loop()
   distance = ultrasonic.calcDistance();
   publishMessage();
   connect_aws._client.loop();
+  ota.loopingOTA();
   delay(1000);
 }
