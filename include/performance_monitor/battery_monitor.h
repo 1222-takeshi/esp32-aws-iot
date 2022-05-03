cd0aba028ddaf3b777c057132d0370ffb37f0040
Takeshi Miura <57553950+1222-takeshi@users.noreply.github.com>
2022-05-03 21:15:03 +0900

    add battery monitor (#25)
    * fix lost connection to AWS
    
    * add battery monitor
    
    * add wired images

--- include/performance_monitor/battery_monitor.h
+++ include/performance_monitor/battery_monitor.h
@@ -1,27 +1,23 @@
-// /*
-//  * Connect_AWS.h
-//  */
-// #ifndef CONNECT_AWS_CONNECT_AWS_H_
-// #define CONNECT_AWS_CONNECT_AWS_H_
-// #include <Arduino.h>
+/*
+ * battery_monitor.h
+ */
+#ifndef PERFORMANCE_MONITOR_BATTERY_MONITOR_H_
+#define PERFORMANCE_MONITOR_BATTERY_MONITOR_H_
+#include <Arduino.h>
 
-// #include "secrets.h"
-// #include <WiFiClientSecure.h>
-// #include <MQTTClient.h>
-// #include <ArduinoJson.h>
-// #include "WiFi.h"
-// #include <WiFiUdp.h>
-// class ConnectAWS
-// {
-// public:
-//   ConnectAWS(String pub_topic, String sub_topic);
-//   void connectToAWS();
-//   MQTTClient _client;
+#include <esp_task_wdt.h>
+#include "performance_monitor/task_manager.h"
+class BatteryMonitor
+{
+public:
+  BatteryMonitor(uint8_t adcPin);
+  void getCurrentVoltage();
+  void setup();
+  void loop();
+  uint8_t adc_pin_;
+  float ADC_value_;
+  float voltage_;
+  float battery_;
+};
 
-// private:
-//   String _pub_topic;
-//   String _sub_topic;
-
-// };
-
-// #endif  // CONNECT_AWS_CONNECT_AWS_H_
+#endif  // CONNECT_AWS_CONNECT_AWS_H_
